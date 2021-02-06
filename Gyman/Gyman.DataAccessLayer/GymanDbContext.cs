a0513bf5a4fd034bf8fb99543be55d0c1e4e56bd
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 16:25:59 +0300

    Create GymanDbContext

---
+++ Gyman/Gyman.DataAccessLayer/GymanDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Gyman.BusinessLogicLayer;
+
+namespace Gyman.DataAccessLayer
+{
+    public class GymanDbContext : DbContext
+    {
+        public GymanDbContext()
+            : base("GymanDb")
+        { }
+
+        public virtual DbSet<Member> Members { get; set; }
+
+        public virtual DbSet<Trainer> Trainers { get; set; }
+
+        public virtual DbSet<Training> Trainings { get; set; }
+
+        public virtual DbSet<Subscription> Subscriptions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
