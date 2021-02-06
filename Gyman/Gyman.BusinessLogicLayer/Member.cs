f72e35b08f916e192a2e4cab534fee6ae03b6f0e
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 15:57:19 +0300

    Add new subscription and  member models

--- Gyman/Gyman.BusinessLogicLayer/Member.cs
+++ Gyman/Gyman.BusinessLogicLayer/Member.cs
@@ -1,6 +1,40 @@
-﻿namespace Gyman.BusinessLogicLayer
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gyman.BusinessLogicLayer
 {
     public class Member
     {
+        public Member() { }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        public double Height { get; set; }
+
+        public bool HasSubscription { get; set; }
+
+        public int SubscriptionId { get; set; }
+
+        public Subscription Subscription { get; set; }
     }
 }
---
+++ Gyman/Gyman.BusinessLogicLayer/Subscription.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Gyman.BusinessLogicLayer
+{
+    public enum SubscriptionType
+    {
+        None,
+        S,
+        M,
+        L
+    }
+
+    public class Subscription
+    {
+        public Subscription() { }
+
+        public int Id { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public SubscriptionType SubscriptionType { get; set; }
+    }
+}
