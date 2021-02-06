9f8d4417cc4474f33fddbfd6f94fd0aa4417b38b
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 15:37:15 +0300

    Add a new training model wrapper

---
+++ Gyman/Gyman.PresentationLayer/Wrappers/TrainingWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Gyman.BusinessLogicLayer;
+
+namespace Gyman.PresentationLayer.Wrappers
+{
+    public class TrainingWrapper : ModelWrapper<Training>
+    {
+        public TrainingWrapper(Training model)
+            : base(model)
+        { }
+
+        public int Id => Model.Id;
+
+        public DateTime Start
+        {
+            get => GetValue<DateTime>();
+            set => SetValue(value);
+        }
+
+        public DateTime End
+        {
+            get => GetValue<DateTime>();
+            set => SetValue(value);
+        }
+
+        public int TrainerId
+        {
+            get => GetValue<int>();
+            set => SetValue(value);
+        }
+
+        public int MemberId
+        {
+            get => GetValue<int>();
+            set => SetValue(value);
+        }
+    }
+}
