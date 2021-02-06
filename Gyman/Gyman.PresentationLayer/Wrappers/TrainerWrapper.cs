06d7a2b03c4567f3f14e18c504d0a0c001a30e70
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 15:26:21 +0300

    Add a new trainer model wrapper

---
+++ Gyman/Gyman.PresentationLayer/Wrappers/TrainerWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Gyman.BusinessLogicLayer;
+
+namespace Gyman.PresentationLayer.Wrappers
+{
+    public class TrainerWrapper : ModelWrapper<Trainer>
+    {
+        public TrainerWrapper(Trainer model)
+            : base(model)
+        { }
+
+        public int Id => Model.Id;
+
+        public string Name
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public string Surname
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public string Phone
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public bool IsBusy
+        {
+            get => GetValue<bool>();
+            set => SetValue(value);
+        }
+    }
+}
