5dd4ed512a094f5da563c77b177412332f3dff70
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 16:30:03 +0300

    Enable migrations

---
+++ Gyman/Gyman.DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Gyman.DataAccessLayer.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<GymanDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GymanDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
