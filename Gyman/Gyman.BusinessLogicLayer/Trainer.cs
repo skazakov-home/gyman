2666c9bb29134ff32e309d5bb2d683faeadddf12
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-06 15:20:24 +0300

    Add a new trainer model

---
+++ Gyman/Gyman.BusinessLogicLayer/Trainer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gyman.BusinessLogicLayer
+{
+    public class Trainer
+    {
+        public Trainer() { }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+
+        public bool IsBusy { get; set; }
+    }
+}
