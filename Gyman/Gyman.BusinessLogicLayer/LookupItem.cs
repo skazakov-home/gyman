dd0d145ba469d11f7de0d6980a2ef1869d198134
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-21 17:57:20 +0300

    Create LookupItem

---
+++ Gyman/Gyman.BusinessLogicLayer/LookupItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Gyman.BusinessLogicLayer
+{
+    public class LookupItem
+    {
+        public int Id { get; set; }
+        public string DisplayMember { get; set; }
+    }
+
+    public class NullLookupItem : LookupItem
+    {
+        public new int? Id => null;
+    }
+}
