3d03c506f6d2edd9081c01aab9b0dbefa44ed647
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-21 17:35:01 +0300

    Create NavigationItemViewModel

---
+++ Gyman/Gyman.PresentationLayer/ViewModels/NavigationItemViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Input;
+using Gyman.PresentationLayer.Events;
+using Prism.Commands;
+using Prism.Events;
+
+namespace Gyman.PresentationLayer.ViewModels
+{
+    public class NavigationItemViewModel : ViewModelBase
+    {
+        private readonly IEventAggregator eventAggregator;
+        private string displayMember;
+
+        public NavigationItemViewModel(
+            IEventAggregator eventAggregator,
+            int id,
+            string displayMember,
+            string detailViewModelName)
+        {
+            this.eventAggregator = eventAggregator;
+            Id = id;
+            DisplayMember = displayMember;
+            DetailViewModelName = detailViewModelName;
+            OpenDetailViewCommand = new DelegateCommand(OnOpenDetailView);
+        }
+
+        public int Id { get; }
+
+        public string DisplayMember
+        {
+            get => displayMember;
+            set
+            {
+                displayMember = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DetailViewModelName { get; }
+
+        public ICommand OpenDetailViewCommand { get; }
+
+        private void OnOpenDetailView()
+        {
+            eventAggregator.GetEvent<OpenDetailViewEvent>().Publish(
+                new OpenDetailViewEventArgs
+                {
+                    Id = Id,
+                    ViewModelName = DetailViewModelName
+                });
+        }
+    }
+}
