86a7e095f9aa46fe58a41bf628910ae822d257cb
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-22 20:52:21 +0300

    Change bootstrapper

--- Gyman/Gyman.PresentationLayer/Startup/Bootstrapper.cs
+++ Gyman/Gyman.PresentationLayer/Startup/Bootstrapper.cs
@@ -1,7 +1,11 @@
 ﻿using Autofac;
+using Gyman.DataAccessLayer;
+using Gyman.PresentationLayer.Data.Repositories;
+using Gyman.PresentationLayer.Data.Services;
 using Gyman.PresentationLayer.ViewModels;
 using Gyman.PresentationLayer.Views;
 using Gyman.PresentationLayer.Views.Services;
+using Prism.Events;
 
 namespace Gyman.PresentationLayer.Startup
 {
@@ -13,7 +17,13 @@ namespace Gyman.PresentationLayer.Startup
 
             builder.RegisterType<MainView>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<NavigationViewModel>().As<INavigationViewModel>();
+            builder.RegisterType<MemberDetailViewModel>().Keyed<IDetailViewModel>(nameof(MemberDetailViewModel));
+            builder.RegisterType<GymanDbContext>().AsSelf();
             builder.RegisterType<DialogMessageService>().As<IDialogMessageService>();
+            builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
+            builder.RegisterType<MemberRepository>().As<IMemberRepository>();
+            builder.RegisterType<EventAggregator>().As<IEventAggregator>().SingleInstance();
 
             return builder.Build();
         }
