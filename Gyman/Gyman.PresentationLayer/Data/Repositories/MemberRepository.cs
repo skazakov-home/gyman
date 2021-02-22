6c37ff2a19647527ee68ab1975042c3c7e3f4bf9
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-22 20:51:13 +0300

    Create MemberRepository

---
+++ Gyman/Gyman.PresentationLayer/Data/Repositories/MemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using Gyman.BusinessLogicLayer;
+using Gyman.DataAccessLayer;
+
+namespace Gyman.PresentationLayer.Data.Repositories
+{
+    public class MemberRepository : GenericRepository<Member, GymanDbContext>,
+        IMemberRepository
+    {
+        public MemberRepository(GymanDbContext context) : base(context) { }
+
+        public override async Task<Member> GetByIdAsync(int memberId)
+        {
+            return await Context.Members.SingleAsync(m => m.Id == memberId);
+        }
+    }
+}
