b8627424874c4370ac5c96e1b9a8953e579e6e9b
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-22 20:51:32 +0300

    Create LookupDataService

---
+++ Gyman/Gyman.PresentationLayer/Data/Services/LookupDataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Gyman.BusinessLogicLayer;
+using Gyman.DataAccessLayer;
+
+namespace Gyman.PresentationLayer.Data.Services
+{
+    public class LookupDataService : IMemberLookupDataService
+    {
+        private readonly Func<GymanDbContext> contextCreator;
+
+        public LookupDataService(Func<GymanDbContext> contextCreator)
+        {
+            this.contextCreator = contextCreator;
+        }
+
+        public async Task<IEnumerable<LookupItem>> LoadMemberLookupItemsAsync()
+        {
+            using (var context = contextCreator())
+            {
+                return await context.Members.AsNoTracking()
+                    .Select(member => new LookupItem
+                    {
+                        Id = member.Id,
+                        DisplayMember = member.Name + " " + member.Surname
+                    })
+                    .ToListAsync();
+            }
+        }
+    }
+}
