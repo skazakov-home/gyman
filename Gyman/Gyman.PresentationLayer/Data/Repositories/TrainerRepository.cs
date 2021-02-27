61ffbe19ea6a7f84b1e63a7e3273bc26a8847752
Stanislav Kazakov <skazakov.home@gmail.com>
2021-02-27 19:29:41 +0300

    Create trainer repository

---
+++ Gyman/Gyman.PresentationLayer/Data/Repositories/TrainerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using Gyman.BusinessLogicLayer;
+using Gyman.DataAccessLayer;
+
+namespace Gyman.PresentationLayer.Data.Repositories
+{
+    public class TrainerRepository : GenericRepository<Trainer, GymanDbContext>,
+        ITrainerRepository
+    {
+        public TrainerRepository(GymanDbContext context) : base(context) { }
+
+        public override async Task<Trainer> GetByIdAsync(int trainerId)
+        {
+            return await Context.Trainers.SingleAsync(m => m.Id == trainerId);
+        }
+    }
+}
