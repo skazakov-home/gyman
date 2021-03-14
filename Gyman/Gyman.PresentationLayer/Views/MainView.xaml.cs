e7bc69d56609e20efca21df0936d3fb4cb4af90f
Stanislav Kazakov <skazakov.home@gmail.com>
2021-03-14 18:37:38 +0300

    Change user interface

--- Gyman/Gyman.PresentationLayer/Views/MainView.xaml.cs
+++ Gyman/Gyman.PresentationLayer/Views/MainView.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System.Windows;
 using Gyman.PresentationLayer.ViewModels;
+using MahApps.Metro.Controls;
 
 namespace Gyman.PresentationLayer.Views
 {
-    public partial class MainView : Window
+    public partial class MainView : MetroWindow
     {
         private readonly MainViewModel mainViewModel;
 
