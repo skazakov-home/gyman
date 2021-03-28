4a736c291762af5db7f8f99730f7b9126514c848
Stanislav Kazakov <skazakov.home@gmail.com>
2021-03-28 17:54:51 +0300

    Resolve training dependencies

--- Gyman/Gyman.PresentationLayer/Startup/Bootstrapper.cs
+++ Gyman/Gyman.PresentationLayer/Startup/Bootstrapper.cs
@@ -20,15 +20,22 @@ namespace Gyman.PresentationLayer.Startup
             builder.RegisterType<NavigationViewModel>().As<INavigationViewModel>();
             builder.RegisterType<MemberDetailViewModel>().Keyed<ITabViewModel>(nameof(MemberDetailViewModel));
             builder.RegisterType<TrainerDetailViewModel>().Keyed<ITabViewModel>(nameof(TrainerDetailViewModel));
+            builder.RegisterType<TrainingDetailViewModel>().Keyed<ITabViewModel>(nameof(TrainingDetailViewModel));
             builder.RegisterType<SubscriptionDetailViewModel>().Keyed<ITabViewModel>(nameof(SubscriptionDetailViewModel));
             builder.RegisterType<MembersViewModel>().Keyed<ITabViewModel>(nameof(MembersViewModel));
             builder.RegisterType<TrainersViewModel>().Keyed<ITabViewModel>(nameof(TrainersViewModel));
+
             builder.RegisterType<GymanDbContext>().AsSelf();
+
             builder.RegisterType<DialogMessageService>().As<IDialogMessageService>();
+
             builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
+
             builder.RegisterType<MemberRepository>().As<IMemberRepository>();
             builder.RegisterType<TrainerRepository>().As<ITrainerRepository>();
+            builder.RegisterType<TrainingRepository>().As<ITrainingRepository>();
             builder.RegisterType<SubscriptionRepository>().As<ISubscriptionRepository>();
+
             builder.RegisterType<EventAggregator>().As<IEventAggregator>().SingleInstance();
 
             return builder.Build();
