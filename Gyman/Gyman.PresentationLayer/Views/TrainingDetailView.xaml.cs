661e838c5ce64cc3b1495c0320f94bf7130b38f4
Stanislav Kazakov <skazakov.home@gmail.com>
2021-03-28 17:49:09 +0300

    Add trainingdetailview

---
+++ Gyman/Gyman.PresentationLayer/Views/TrainingDetailView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Gyman.PresentationLayer.Views
+{
+    public partial class TrainingDetailView : UserControl
+    {
+        public TrainingDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
